61b298b4ee892043a2de63e2302750f2007ddd39
Rajan4405 <rajanshatma652@gmail.com>
2023-03-09 09:10:22 +0545

    Async Await Example

--- Asynchronous-JS/index.js
+++ Asynchronous-JS/index.js
@@ -19,6 +19,49 @@ const writeFilePro = (file, data) => {
   })
 }
 
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog image save to file');
+  } catch (err) {
+    console.log(err);
+    throw (err);
+  }
+  return '2: Ready 🐕'
+}
+
+(async () => {
+  try {
+    console.log('1: Will get dog pics!')
+    const x = await getDogPic();
+    console.log(x)
+    console.log('3: Done getting dog pics!')
+
+
+  } catch (err) {
+    console.log('ERROR 💥')
+  }
+})();
+
+/*
+console.log('1: Will get dog pics!')
+getDogPic().then(x => {
+  console.log(x)
+  console.log('3: Done getting dog pics!')
+}).catch(err => console.log('ERROR 💥'))
+*/
+
+
+
+// Promise
+/*
+
 readFilePro(`${__dirname}/dog.txt`).then(data => {
   console.log(`Breed: ${data}`);
 
@@ -34,5 +77,4 @@ readFilePro(`${__dirname}/dog.txt`).then(data => {
     console.log('Random dog image save to file');
 
   }).catch(err => console.log(err.message));
-
-
+*/
