3151fa2738a337e079c652b05fe29d65dfce62ee
Rajan4405 <rajanshatma652@gmail.com>
2023-04-11 21:54:24 +0545

    Creating Mongo Connection

--- Natouras/server.js
+++ Natouras/server.js
@@ -1,11 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 dotenv.config({ path: './config.env' })
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-
-// console.log(app.get('env'))
-// console.log(process.env)
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+  .then(() => console.log('DB connection successful!'))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
