2ba0491aca13bcffd73e75efff7649e4d3a9d56b
Rajan4405 <rajanshatma652@gmail.com>
2023-04-15 20:26:03 +0545

    fix error

--- Natouras/controllers/tourController.js
+++ Natouras/controllers/tourController.js
@@ -8,8 +8,6 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 }
 
-
-
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
@@ -164,4 +162,54 @@ exports.deleteTour = async (req, res) => {
     })
   }
 
+}
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+      // {
+      //   $match: { _id: { $ne: 'EASY' } }
+      // }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 }
